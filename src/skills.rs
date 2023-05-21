c502b377bb1237a279e635f180c1665857706200
Dejan FAJFAR <dejan@fajfar.com>
2023-05-21 22:47:43 +0200

    hit probability looks better now

--- src/skills.rs
+++ src/skills.rs
@@ -20,18 +20,41 @@ impl Skills {
     }
 
     fn calculate_hit_probability(&self, min_requirements: Skills) -> f64 {
-        // does not meet minimal requirements
-        if self.dexterity() < min_requirements.dexterity() {
-            let gap = min_requirements.dexterity - self.dexterity();
-
-            return 0.5 - ((f64::from(gap.raw_value()) / 10.0) * 0.5);
+        // this one special case where you need max dex to wield a weapon an you have max dex, then you should have a hit!
+        if self.dexterity() == min_requirements.dexterity() && min_requirements.dexterity() == SkillPoint::MAX {
+            return 1.0;
         }
 
-        // meets minimal requirements
+        // if the player is underleveled
+        // always less than 50%
+        if self.dexterity() < min_requirements.dexterity() {
+            let gap: SkillPoint = min_requirements.dexterity - self.dexterity();
 
+            match gap.raw_value() {
+                1 => return 0.45,
+                2 => return 0.4,
+                3 => return 0.3,
+                4 => return 0.2,
+                _ => return 0.1
+            }
+        }
+
+        // if payer dex and min req dex are the same the we have 50% chance
+        if self.dexterity() == min_requirements.dexterity() {
+            return 0.5;
+        }
+
+        // if the player is overleveled
+        // always more than 50%
         let gap: SkillPoint = self.dexterity() - min_requirements.dexterity();
 
-        return (f64::from(gap.raw_value()) / 10.0) + 0.5;
+        match gap.raw_value() {
+            1 => return 0.55,
+            2 => return 0.6,
+            3 => return 0.7,
+            4 => return 0.8,
+            _ => return 0.9
+        };
     }
 
     pub fn is_hit(&self, min_requirements: Option<Skills>) -> bool {
@@ -102,6 +125,7 @@ mod tests {
     #[test]
     fn calculate_hit_probability_tests() {
         // if required and player stat same then the probability is 0.5
+        assert_relative_eq!(0.5, call_calculate_hit_probability(0, 0));
         assert_eq!(0.5, call_calculate_hit_probability(3, 3));
         assert_eq!(0.5, call_calculate_hit_probability(4, 4));
         assert_eq!(0.5, call_calculate_hit_probability(5, 5));
@@ -109,77 +133,75 @@ mod tests {
         assert_eq!(0.5, call_calculate_hit_probability(7, 7));
         assert_eq!(0.5, call_calculate_hit_probability(8, 8));
         assert_eq!(0.5, call_calculate_hit_probability(9, 9));
+        assert_eq!(1.0, call_calculate_hit_probability(10, 10));
 
-        // required is larger than the player
-        assert_relative_eq!(
-            0.45,
-            call_calculate_hit_probability(3, 4),
-            max_relative = 0.01
-        );
-        assert_relative_eq!(
-            0.40,
-            call_calculate_hit_probability(3, 5),
-            max_relative = 0.01
-        );
-        assert_relative_eq!(
-            0.35,
-            call_calculate_hit_probability(3, 6),
-            max_relative = 0.01
-        );
-        assert_relative_eq!(
-            0.30,
-            call_calculate_hit_probability(3, 7),
-            max_relative = 0.01
-        );
-        assert_relative_eq!(
-            0.25,
-            call_calculate_hit_probability(3, 8),
-            max_relative = 0.01
-        );
-        assert_relative_eq!(
-            0.20,
-            call_calculate_hit_probability(3, 9),
-            max_relative = 0.01
-        );
+        assert_relative_eq!(0.45, call_calculate_hit_probability(0, 1), max_relative = 0.01);
+        assert_relative_eq!(0.4, call_calculate_hit_probability(0, 2), max_relative = 0.01);
 
-        // required is smaller that the player
-
+        // middle sample Player dex = 5
         assert_relative_eq!(
-            0.60,
-            call_calculate_hit_probability(4, 3),
+            0.9,
+            call_calculate_hit_probability(5, 0),
             max_relative = 0.01
         );
         assert_relative_eq!(
-            0.70,
+            0.8,
+            call_calculate_hit_probability(5, 1),
+            max_relative = 0.01
+        );
+        assert_relative_eq!(
+            0.7,
+            call_calculate_hit_probability(5, 2),
+            max_relative = 0.01
+        );
+        assert_relative_eq!(
+            0.6,
             call_calculate_hit_probability(5, 3),
             max_relative = 0.01
         );
         assert_relative_eq!(
-            0.75,
-            call_calculate_hit_probability(6, 3),
+            0.55,
+            call_calculate_hit_probability(5, 4),
             max_relative = 0.01
         );
         assert_relative_eq!(
-            0.80,
-            call_calculate_hit_probability(7, 3),
+            0.5,
+            call_calculate_hit_probability(5, 5),
             max_relative = 0.01
         );
         assert_relative_eq!(
-            0.85,
-            call_calculate_hit_probability(8, 3),
+            0.45,
+            call_calculate_hit_probability(5, 6),
             max_relative = 0.01
         );
         assert_relative_eq!(
-            0.90,
-            call_calculate_hit_probability(9, 3),
+            0.4,
+            call_calculate_hit_probability(5, 7),
             max_relative = 0.01
         );
+        assert_relative_eq!(
+            0.3,
+            call_calculate_hit_probability(5, 8),
+            max_relative = 0.01
+        );
+        assert_relative_eq!(
+            0.2,
+            call_calculate_hit_probability(5, 9),
+            max_relative = 0.01
+        );
+        assert_relative_eq!(
+            0.1,
+            call_calculate_hit_probability(5, 10),
+            max_relative = 0.01
+        );
+
+        
     }
 
     #[test]
     fn foo(){
-        for my_dex in 0..10 {
-            for other_dex in 0..10 {
+        for my_dex in 0..=10 {
+            for other_dex in 0..=10 {
                 println!("me: {0}, min: {1} => {2}", my_dex, other_dex, call_calculate_hit_probability(my_dex, other_dex));
             }
         }
